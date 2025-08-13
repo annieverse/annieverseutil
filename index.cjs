338693adc65cf23cafecf1157dec346065d7df65
Andrew <thefryingpan2021@gmail.com>
2025-08-12 20:24:22 -0400

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -1,11 +1,10 @@
 "use strict";
-const { findBestMatch } = require("./string-similarity/findBestMatch.cjs");
-const { compareTwoStrings } = require("./string-similarity/compareTwoStrings.cjs");
-const { downloadPixivImage } = require("./pixiv/pixivImg.cjs");
+const findBestMatch = require("./string-similarity/findBestMatch.cjs");
+const compareTwoStrings = require("./string-similarity/compareTwoStrings.cjs");
+const downloadPixivImage = require("./pixiv/pixivImg.cjs");
 
 
 const stringSimilarity = { compareTwoStrings, findBestMatch };
 const pixiv = { downloadPixivImage };
 
-module.exports = stringSimilarity;
-module.exports = pixiv;
+module.exports = { stringSimilarity, pixiv };
