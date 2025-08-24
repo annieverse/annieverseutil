e355d39cebd2958579b550b0f6543dfd309af12d
Andrew <thefryingpan2021@gmail.com>
2025-08-24 03:21:15 -0400

    fix(): init commit

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,7 @@ import { findBestMatch, compareTwoStrings } from "./modules/string-similarity/fi
 import { downloadPixivImage } from "./modules/pixiv/pixivImg";
 import { chunkOptions } from "./modules/general-util/chunkOptions";
 import { closestBelow, commanifier, formatK, getNumberInRange, random, trueInt } from "./modules/general-util/numberRelated";
-
 const stringSimilarity = { compareTwoStrings, findBestMatch }
 const pixiv = { downloadPixivImage };
 const utils = { chunkOptions, closestBelow, commanifier, formatK, getNumberInRange, random, trueInt };
-
 export { stringSimilarity, pixiv, utils };
